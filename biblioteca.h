33a2ed08cb21783cd2a227df11ada7350e1c3b10
Mathias2626 <Mathias.zea@udla.edu.ec>
2025-12-12 14:10:21 -0500

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -1,4 +1,3 @@
-// biblioteca.h - Archivo de cabecera
 #define MAX_TITULO 100
 #define MAX_AUTOR 50
 #define MAX_LIBROS 10
@@ -18,5 +17,5 @@ void mostrarLibros(Libro libros[], int total);
 void buscarLibro(Libro libros[], int total);
 void cambiarEstado(Libro libros[], int total);
 void eliminarLibro(Libro libros[], int *total);
-
 int idExiste(Libro libros[], int total, int id);
+
