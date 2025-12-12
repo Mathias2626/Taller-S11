a31c3ee3504673cff95774261e77d61321a1348c
Mathias2626 <Mathias.zea@udla.edu.ec>
2025-12-12 13:57:42 -0500

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -3,13 +3,14 @@
 #define MAX_AUTOR 50
 #define MAX_LIBROS 10
 
-typedef struct {
+struct Libro 
+{
     int id;
     char titulo[MAX_TITULO];
     char autor[MAX_AUTOR];
     int anio;
     char estado[15];
-} Libro;
+};
 
 // Funciones
 void agregarLibro(Libro libros[], int *total);
@@ -17,4 +18,5 @@ void mostrarLibros(Libro libros[], int total);
 void buscarLibro(Libro libros[], int total);
 void cambiarEstado(Libro libros[], int total);
 void eliminarLibro(Libro libros[], int *total);
+
 int idExiste(Libro libros[], int total, int id);
